1e49e801bb4602a79705a8c4c29d0a11c3486de8
Amila Welihinda <amilajack@gmail.com>
2017-07-26 14:03:14 -0700

    Initial example commit

--- app/index.js
+++ app/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
+import opencv from 'opencv';
 import { AppContainer } from 'react-hot-loader';
 import Root from './containers/Root';
 import { configureStore, history } from './store/configureStore';
 import './app.global.css';
 
 const store = configureStore();
+
+opencv.readImage('./img/mona.png', (err, im) => {
+  im.detectObject(opencv.FACE_CASCADE, {}, (err, faces) => {
+    console.log(err);
+    for (let i = 0; i < faces.length; i++) {
+      const x = faces[i];
+      im.ellipse(x.x + x.width / 2, x.y + x.height / 2, x.width / 2, x.height / 2);
+    }
+    im.save('./out.jpg');
+  });
+});
 
 render(
   <AppContainer>
